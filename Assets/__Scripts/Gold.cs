e5919e48a46102e7b17856645878066387a4f75a
Nguyen Phan <nqphan212@gmail.com>
2016-06-21 09:33:56 -0400

    create IceCubeBreaking and GoldBreaking particles

---
+++ Assets/__Scripts/Gold.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gold : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		gameObject.SetActive(false);
+	}
+}
